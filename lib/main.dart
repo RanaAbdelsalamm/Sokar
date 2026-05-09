7aa0d85d451841596a0868585430fd811a6c37d2
Rana Abd El-Salam Ahmed <ranabdelsalamm@gmail.com>
2026-05-09 23:47:39 +0300

    feat: initialize firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
